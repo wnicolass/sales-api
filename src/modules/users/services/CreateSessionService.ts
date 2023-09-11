d5031dfcfee82ff2140d6088ba6811d1838dc126
wnicolass <nicolaswgl99@gmail.com>
2023-09-11 20:42:31 +0100

    feat: generate jwt to authenticate users

--- src/modules/users/services/CreateSessionService.ts
+++ src/modules/users/services/CreateSessionService.ts
@@ -1,16 +1,26 @@
-import { getCustomRepository } from 'typeorm';
-import { AppError } from '@shared/errors/AppError';
-import { User } from '../typeorm/entities/User';
-import { UserRepository } from '../typeorm/repositories/UserRepository';
+import { sign } from 'jsonwebtoken';
 import { compare } from 'bcrypt';
+import { getCustomRepository } from 'typeorm';
+import { User } from '../typeorm/entities/User';
+import { AppError } from '@shared/errors/AppError';
+import { tokenConfig } from '@config/token';
+import { UserRepository } from '../typeorm/repositories/UserRepository';
 
 interface IUserRequest {
   email: string;
   password: string;
 }
 
+interface ISessionResponse {
+  user: User;
+  token: string;
+}
+
 export class CreateSessionService {
-  public async execute({ email, password }: IUserRequest): Promise<User> {
+  public async execute({
+    email,
+    password,
+  }: IUserRequest): Promise<ISessionResponse> {
     const userRepository = getCustomRepository(UserRepository);
     const user = await userRepository.findByEmail(email);
 
@@ -24,6 +34,11 @@ export class CreateSessionService {
       throw new AppError('Wrong email/password combination', 401);
     }
 
-    return user;
+    const token = sign({}, tokenConfig.secret, {
+      subject: user.user_id,
+      expiresIn: tokenConfig.exp,
+    });
+
+    return { user, token };
   }
 }
