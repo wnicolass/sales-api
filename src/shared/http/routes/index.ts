7317867a24674834bc0cc7a15425a325bbcabb8b
wnicolass <nicolaswgl99@gmail.com>
2023-09-11 18:12:54 +0100

    feat: attach 'usersRouter' to base router

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
+import { usersRouter } from '@modules/users/routes/users.routes';
 import { productsRouter } from '@modules/products/routes/products.routes';
 
 const router = Router();
 
+router.use('/users', usersRouter);
 router.use('/products', productsRouter);
 
 export default router;
