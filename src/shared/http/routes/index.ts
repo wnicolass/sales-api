c3b6a68b349aea8e4fa132f00701756355259392
wnicolass <nicolaswgl99@gmail.com>
2023-09-12 17:40:48 +0100

    fix: change filename

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -2,7 +2,7 @@ import { Router } from 'express';
 import { usersRouter } from '@modules/users/routes/users.routes';
 import { productsRouter } from '@modules/products/routes/products.routes';
 import { sessionsRouter } from '@modules/users/routes/sessions.routes';
-import { passwordsRouter } from '@modules/users/routes/password.routes';
+import { passwordsRouter } from '@modules/users/routes/passwords.routes';
 
 const router = Router();
 
