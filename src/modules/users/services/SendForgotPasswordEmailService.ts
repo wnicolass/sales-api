61183a021584260d31800eaf8d6686fcfecb0a1f
wnicolass <nicolaswgl99@gmail.com>
2023-09-13 17:26:15 +0100

    feat: configure 'SendForgotPasswordEmailService' to work with template files

--- src/modules/users/services/SendForgotPasswordEmailService.ts
+++ src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -1,5 +1,7 @@
+import { resolve } from 'node:path';
 import { getCustomRepository } from 'typeorm';
 import { AppError } from '@shared/errors/AppError';
+import { EtherealMail } from '@config/mail/EtherealMail';
 import { UserRepository } from '../typeorm/repositories/UserRepository';
 import { UserTokenRepository } from '../typeorm/repositories/UserToken';
 
@@ -18,6 +20,32 @@ export class SendForgotPasswordEmailService {
       throw new AppError('User not found', 404);
     }
 
-    await userTokenRepository.generate(user.user_id);
+    const { token } = await userTokenRepository.generate(user.user_id);
+
+    const forgotPasswordTemplate = resolve(
+      __dirname,
+      '..',
+      'views',
+      'forgot-password.hbs',
+    );
+
+    await EtherealMail.sendMail({
+      to: {
+        name: user.username,
+        address: user.email,
+      },
+      from: {
+        name: 'Team Sales API',
+        address: 'team@salesapi.pt',
+      },
+      subject: '[Sales API] Reset your password',
+      templateData: {
+        file: forgotPasswordTemplate,
+        variables: {
+          name: user.username,
+          link: `http://localhost:3000/reset_password?token=${token}`,
+        },
+      },
+    });
   }
 }
