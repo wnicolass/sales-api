2a687bf63fb34418ac2117e473e0c2728e1b85b1
wnicolass <nicolaswgl99@gmail.com>
2023-09-13 23:45:51 +0100

    feat: create profile router and attach it to base router

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -3,6 +3,7 @@ import { usersRouter } from '@modules/users/routes/users.routes';
 import { productsRouter } from '@modules/products/routes/products.routes';
 import { sessionsRouter } from '@modules/users/routes/sessions.routes';
 import { passwordsRouter } from '@modules/users/routes/passwords.routes';
+import { usersProfilesRouter } from '@modules/users/routes/profiles.routes';
 
 const router = Router();
 
@@ -10,5 +11,6 @@ router.use('/users', usersRouter);
 router.use('/sessions', sessionsRouter);
 router.use('/products', productsRouter);
 router.use('/passwords', passwordsRouter);
+router.use('/profile', usersProfilesRouter);
 
 export default router;
