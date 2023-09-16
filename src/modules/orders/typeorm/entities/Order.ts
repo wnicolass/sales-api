058d9b7f5221fc42216854cd8834f6560808ed67
wnicolass <nicolaswgl99@gmail.com>
2023-09-16 18:44:42 +0100

    feat: create 'Order' entity

---
+++ src/modules/orders/typeorm/entities/Order.ts
@@ -0,0 +1,25 @@
+import { Customer } from '@modules/customers/typeorm/entities/Customer';
+import {
+  CreateDateColumn,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity('order')
+export class Order {
+  @PrimaryGeneratedColumn('uuid')
+  order_id: string;
+
+  @ManyToOne(() => Customer)
+  @JoinColumn({ name: 'customer_id' })
+  customer: Customer;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+}
