28ea1392069783c1d4faf31f65c2053cd99c9a55
wnicolass <nicolaswgl99@gmail.com>
2023-09-16 20:51:06 +0100

    feat: attach orders router to base router

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -5,10 +5,12 @@ import { sessionsRouter } from '@modules/users/routes/sessions.routes';
 import { passwordsRouter } from '@modules/users/routes/passwords.routes';
 import { usersProfilesRouter } from '@modules/users/routes/profiles.routes';
 import { customersRouter } from '@modules/customers/routes/customers.routes';
+import { ordersRouter } from '@modules/orders/routes/orders.routes';
 
 const router = Router();
 
 router.use('/users', usersRouter);
+router.use('/orders', ordersRouter);
 router.use('/sessions', sessionsRouter);
 router.use('/products', productsRouter);
 router.use('/passwords', passwordsRouter);
