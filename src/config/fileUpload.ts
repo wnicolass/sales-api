8c19d0fb091301ccc7f8df28b71ace12315b1f7a
wnicolass <nicolaswgl99@gmail.com>
2023-09-21 22:49:54 +0100

    feat: add temp folder to 'uploadConfig'

--- src/config/fileUpload.ts
+++ src/config/fileUpload.ts
@@ -4,11 +4,13 @@ import { diskStorage } from 'multer';
 
 function setupMulter() {
   const uploadsFolder = resolve(__dirname, '..', '..', 'uploads');
+  const tempFolder = resolve(__dirname, '..', '..', 'temp');
 
   return {
+    tempFolder,
     directory: uploadsFolder,
     storage: diskStorage({
-      destination: uploadsFolder,
+      destination: tempFolder,
       filename(request, file, callback) {
         const bytesAsHex = randomBytes(8).toString('hex');
         const filename = `${bytesAsHex}-${file.originalname}`;
