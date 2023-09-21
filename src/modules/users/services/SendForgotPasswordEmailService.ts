fb85e71dd70acdacea2b04ab30913ed179b4f085
wnicolass <nicolaswgl99@gmail.com>
2023-09-21 22:10:26 +0100

    feat: update 'SendForgotPasswordEmailService' to use ses or ethereal

--- src/modules/users/services/SendForgotPasswordEmailService.ts
+++ src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -1,6 +1,8 @@
 import { resolve } from 'node:path';
 import { getCustomRepository } from 'typeorm';
+import { SESMail } from '@config/mail/SESMail';
 import { AppError } from '@shared/errors/AppError';
+import { mailConfig } from '@config/mail/mail';
 import { EtherealMail } from '@config/mail/EtherealMail';
 import { UserRepository } from '../typeorm/repositories/UserRepository';
 import { UserTokenRepository } from '../typeorm/repositories/UserToken';
@@ -29,21 +31,17 @@ export class SendForgotPasswordEmailService {
       'forgot-password.hbs',
     );
 
-    await EtherealMail.sendMail({
+    await (mailConfig.driver === 'ses' ? SESMail : EtherealMail).sendMail({
       to: {
         name: user.username,
         address: user.email,
-      },
-      from: {
-        name: 'Team Sales API',
-        address: 'team@salesapi.pt',
       },
       subject: '[Sales API] Reset your password',
       templateData: {
         file: forgotPasswordTemplate,
         variables: {
           name: user.username,
-          link: `http://localhost:3000/reset_password?token=${token}`,
+          link: `${process.env.APP_URL}:${process.env.PORT}/reset_password?token=${token}`,
         },
       },
     });
