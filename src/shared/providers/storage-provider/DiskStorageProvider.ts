3d26c2af01f1cfb51a639b0b8bd7d86baf5c697e
wnicolass <nicolaswgl99@gmail.com>
2023-09-21 22:51:12 +0100

    feat: create 'DiskStorageProvider'

--- src/shared/providers/storage-provider/DiskStorageProvider.ts
+++ src/shared/providers/storage-provider/DiskStorageProvider.ts
@@ -1,7 +1,24 @@
+import { resolve } from 'node:path';
+import { existsSync } from 'node:fs';
+import { rename, unlink } from 'node:fs/promises';
+import { multerConfig } from '@config/fileUpload';
+
 export class DiskStorageProvider {
-  public async saveFile(file: string): Promise<string> {
-    return '';
+  public async saveFile(filename: string): Promise<string> {
+    await rename(
+      resolve(multerConfig.tempFolder, filename),
+      resolve(multerConfig.directory, filename),
+    );
+
+    return filename;
   }
 
-  public async deleteFile(file: string): Promise<void> {}
+  public async deleteFile(filename: string): Promise<void> {
+    const filePath = resolve(multerConfig.directory, filename);
+    const fileExists = existsSync(filePath);
+
+    if (fileExists) {
+      await unlink(filePath);
+    }
+  }
 }
