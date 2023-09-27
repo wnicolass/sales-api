1ddd39185fa32bfdc68cad30100c1ab394e3b12a
wnicolass <nicolaswgl99@gmail.com>
2023-09-27 21:56:35 +0100

    feat: add 'findAll' method to ICustomerRepository

--- src/modules/customers/domain/interfaces/ICustomerRepository.ts
+++ src/modules/customers/domain/interfaces/ICustomerRepository.ts
@@ -1,7 +1,14 @@
 import { ICreateCustomer } from './ICreateCustomer';
 import { ICustomer } from './ICustomer';
+import { ICustomerPagination } from './ICustomerPagination';
 
+export interface FindParams {
+  page: number;
+  skip: number;
+  take: number;
+}
 export interface ICustomerRepository {
+  findAll({ page, skip, take }: FindParams): Promise<ICustomerPagination>;
   findByEmail(email: string): Promise<ICustomer | undefined>;
   findByUsername(username: string): Promise<ICustomer | undefined>;
   findById(customerId: string): Promise<ICustomer | undefined>;
