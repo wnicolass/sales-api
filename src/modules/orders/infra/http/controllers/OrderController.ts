2ef074f383c66992442c13c67de6d9173b6a81c5
wnicolass <nicolaswgl99@gmail.com>
2023-09-29 22:01:20 +0100

    refactor: make 'OrderController' use tsyringe container for dep injection

--- src/modules/orders/infra/http/controllers/OrderController.ts
+++ src/modules/orders/infra/http/controllers/OrderController.ts
@@ -1,12 +1,24 @@
 import { container } from 'tsyringe';
 import { Request, Response } from 'express';
 import { ShowOrderService } from '../../../services/ShowOrderService';
+import { ListOrderService } from '@modules/orders/services/ListOrderService';
 import { CreateOrderService } from '../../../services/CreateOrderService';
 
 export class OrderController {
+  public async index(request: Request, response: Response): Promise<Response> {
+    const { page, limit } = request.query;
+    const listOrders = container.resolve(ListOrderService);
+    const orders = await listOrders.execute({
+      page: page ? +page : 1,
+      limit: limit ? +limit : 15,
+    });
+
+    return response.status(200).json(orders);
+  }
+
   public async show(request: Request, response: Response): Promise<Response> {
     const { orderId } = request.params;
-    const showOrder = new ShowOrderService();
+    const showOrder = container.resolve(ShowOrderService);
     const order = await showOrder.execute({ orderId });
 
     return response.status(200).json(order);
