2623dfbf837ccbb61e597e2db1853d7f681a2d5a
wnicolass <nicolaswgl99@gmail.com>
2023-09-30 20:42:27 +0100

    feat: register 'Product' and 'User' repositories on tsyringe container

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,7 +1,11 @@
 import { container } from 'tsyringe';
+import { UserRepository } from '@modules/users/infra/typeorm/repositories/UserRepository';
 import { OrderRepository } from '@modules/orders/infra/typeorm/repositories/OrderRepository';
+import { IUserRepository } from '@modules/users/domain/interfaces/IUserRepository';
 import { IOrderRepository } from '@modules/orders/domain/interfaces/IOrderRepository';
+import { ProductRepository } from '@modules/products/infra/typeorm/repositories/ProductRepository';
 import { CustomerRepository } from '@modules/customers/infra/typeorm/repositories/CustomerRepository';
+import { IProductRepository } from '@modules/products/domain/interfaces/IProductRepository';
 import { ICustomerRepository } from '@modules/customers/domain/interfaces/ICustomerRepository';
 
 container.registerSingleton<ICustomerRepository>(
@@ -11,4 +15,12 @@ container.registerSingleton<ICustomerRepository>(
 container.registerSingleton<IOrderRepository>(
   'OrderRepository',
   OrderRepository,
+);
+container.registerSingleton<IProductRepository>(
+  'ProductRepository',
+  ProductRepository,
+);
+container.registerSingleton<IUserRepository>(
+  'ProductRepository',
+  UserRepository,
 );
