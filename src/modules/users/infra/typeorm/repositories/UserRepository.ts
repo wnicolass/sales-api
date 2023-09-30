786b911f62b5fc51f02aed3cc55758d082483aba
wnicolass <nicolaswgl99@gmail.com>
2023-09-30 20:39:09 +0100

    feat: implements 'User' and 'UserToken' protocols on repositories

--- src/modules/users/infra/typeorm/repositories/UserRepository.ts
+++ src/modules/users/infra/typeorm/repositories/UserRepository.ts
@@ -1,17 +1,35 @@
-import { EntityRepository, Repository } from 'typeorm';
+import { Repository, getRepository } from 'typeorm';
 import { User } from '../entities/User';
+import { IUserRepository } from '@modules/users/domain/interfaces/IUserRepository';
+import { ICreateUserRequest } from '@modules/users/domain/interfaces/ICreateUserRequest';
+import { IUser } from '@modules/users/domain/interfaces/IUser';
 
-@EntityRepository(User)
-export class UserRepository extends Repository<User> {
+export class UserRepository implements IUserRepository {
+  constructor(private ormRepo: Repository<User> = getRepository(User)) {}
+
+  public async create({
+    username,
+    email,
+    password,
+  }: ICreateUserRequest): Promise<User> {
+    const user = this.ormRepo.create({ username, email, password });
+
+    return user;
+  }
+
+  public async save(user: IUser): Promise<User> {
+    return await this.ormRepo.save(user);
+  }
+
   public async findByUsername(username: string): Promise<User | undefined> {
-    return await this.findOne({ where: { username } });
+    return await this.ormRepo.findOne({ where: { username } });
   }
 
   public async findById(userId: string): Promise<User | undefined> {
-    return await this.findOne({ where: { user_id: userId } });
+    return await this.ormRepo.findOne({ where: { user_id: userId } });
   }
 
   public async findByEmail(email: string): Promise<User | undefined> {
-    return await this.findOne({ where: { email } });
+    return await this.ormRepo.findOne({ where: { email } });
   }
 }
