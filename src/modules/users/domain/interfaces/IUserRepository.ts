abcdf4f5c2b0ff5e34f50caee16c8d9e7effd95c
wnicolass <nicolaswgl99@gmail.com>
2023-09-30 23:09:50 +0100

    feat: add 'findAll' method to 'UserRepository'

--- src/modules/users/domain/interfaces/IUserRepository.ts
+++ src/modules/users/domain/interfaces/IUserRepository.ts
@@ -1,7 +1,10 @@
-import { ICreateUserRequest } from './ICreateUserRequest';
 import { IUser } from './IUser';
+import { IPagination } from '@shared/interfaces/IPagination';
+import { IPaginationParams } from '@shared/interfaces/IPaginationParams';
+import { ICreateUserRequest } from './ICreateUserRequest';
 
 export interface IUserRepository {
+  findAll({ page, skip, take }: IPaginationParams): Promise<IPagination<IUser>>;
   findByUsername(username: string): Promise<IUser | undefined>;
   findById(userId: string): Promise<IUser | undefined>;
   findByEmail(email: string): Promise<IUser | undefined>;
--- src/modules/users/infra/typeorm/repositories/UserRepository.ts
+++ src/modules/users/infra/typeorm/repositories/UserRepository.ts
@@ -3,6 +3,8 @@ import { User } from '../entities/User';
 import { IUserRepository } from '@modules/users/domain/interfaces/IUserRepository';
 import { ICreateUserRequest } from '@modules/users/domain/interfaces/ICreateUserRequest';
 import { IUser } from '@modules/users/domain/interfaces/IUser';
+import { IPaginationParams } from '@shared/interfaces/IPaginationParams';
+import { IPagination } from '@shared/interfaces/IPagination';
 
 export class UserRepository implements IUserRepository {
   constructor(private ormRepo: Repository<User> = getRepository(User)) {}
@@ -19,6 +21,25 @@ export class UserRepository implements IUserRepository {
 
   public async save(user: IUser): Promise<User> {
     return await this.ormRepo.save(user);
+  }
+
+  public async findAll({
+    page,
+    skip,
+    take,
+  }: IPaginationParams): Promise<IPagination<User>> {
+    const [users, count] = await this.ormRepo
+      .createQueryBuilder()
+      .skip(skip)
+      .take(take)
+      .getManyAndCount();
+
+    return {
+      per_page: take,
+      total: count,
+      current_page: page,
+      data: users,
+    };
   }
 
   public async findByUsername(username: string): Promise<User | undefined> {
