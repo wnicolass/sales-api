242beb0813457f596b03e8780eda4fdd064634f5
wnicolass <nicolaswgl99@gmail.com>
2023-09-30 19:52:22 +0100

    refactor: 'ListProductService' by adapting it to dep injection

--- src/modules/products/services/ListProductService.ts
+++ src/modules/products/services/ListProductService.ts
@@ -1,16 +1,35 @@
-import { getCustomRepository } from 'typeorm';
-import { ProductRepository } from '../typeorm/repositories/ProductRepository';
-import { Product } from '../typeorm/entities/Product';
+import { inject, injectable } from 'tsyringe';
+import { IProduct } from '../domain/interfaces/IProduct';
+import { IPagination } from '@shared/interfaces/IPagination';
+import { IProductRepository } from '../domain/interfaces/IProductRepository';
 import { RedisCacheSingleton } from '@shared/cache/RedisCache';
 
+interface ISearchParams {
+  page: number;
+  limit: number;
+}
+
+@injectable()
 export class ListProductsService {
-  public async execute(): Promise<Product[]> {
-    const productRepository = getCustomRepository(ProductRepository);
+  constructor(
+    @inject('ProductRepository') private productRepository: IProductRepository,
+  ) {}
+
+  public async execute({
+    page,
+    limit,
+  }: ISearchParams): Promise<IPagination<IProduct>> {
     const redisCache = RedisCacheSingleton.client;
-    let products = await redisCache.recover<Product[]>('sales-api:products');
+    let products =
+      await redisCache.recover<IPagination<IProduct>>('sales-api:products');
 
     if (!products) {
-      products = await productRepository.find();
+      const queryObject = {
+        take: limit,
+        skip: (+page - 1) * limit,
+        page,
+      };
+      products = await this.productRepository.findAll({ ...queryObject });
       await redisCache.save('sales-api:products', products);
     }
 
