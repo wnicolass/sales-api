3bb74f56e7b7879947ba6efe699e042b61c54aeb
wnicolass <nicolaswgl99@gmail.com>
2023-10-01 15:49:39 +0100

    refactor: 'UserAvatar' controller to use tsyringe container to resolve dep injection

--- src/modules/users/infra/http/controllers/UserAvatarController.ts
+++ src/modules/users/infra/http/controllers/UserAvatarController.ts
@@ -1,3 +1,4 @@
+import { container } from 'tsyringe';
 import { Request, Response } from 'express';
 import { instanceToInstance } from 'class-transformer';
 import { UpdateUserAvatarService } from '../../../services/UpdateUserAvatarService';
@@ -6,7 +7,7 @@ export class UserAvatarController {
   public async update(request: Request, response: Response): Promise<Response> {
     const { userId } = request.user;
     const { filename } = request.file!;
-    const updateAvatar = new UpdateUserAvatarService();
+    const updateAvatar = container.resolve(UpdateUserAvatarService);
     const user = await updateAvatar.execute({ userId, filename });
 
     return response.status(200).json(instanceToInstance(user));
