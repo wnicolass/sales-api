77cfdb6bd544e36bf7f46d427fc236952c9213c2
wnicolass <nicolaswgl99@gmail.com>
2023-10-01 15:49:15 +0100

    refactor: 'Session' controller to use tsyringe container to resolve dep injection

--- src/modules/users/infra/http/controllers/SessionController.ts
+++ src/modules/users/infra/http/controllers/SessionController.ts
@@ -1,3 +1,4 @@
+import { container } from 'tsyringe';
 import { Request, Response } from 'express';
 import { instanceToInstance } from 'class-transformer';
 import { CreateSessionService } from '../../../services/CreateSessionService';
@@ -6,7 +7,7 @@ export class SessionController {
   public async create(request: Request, response: Response): Promise<Response> {
     const { email, password } = request.body;
 
-    const createSession = new CreateSessionService();
+    const createSession = container.resolve(CreateSessionService);
     const user = await createSession.execute({ email, password });
 
     return response.status(200).json(instanceToInstance(user));
