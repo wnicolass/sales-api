2ad8daef435fa9a888fe8040dd01feab6773c5ba
wnicolass <nicolaswgl99@gmail.com>
2023-10-04 23:04:25 +0100

    chore: create datasource configuration

---
+++ src/shared/infra/typeorm/entities/index.ts
@@ -0,0 +1,6 @@
+import { User } from '@modules/users/infra/typeorm/entities/User';
+import { Order } from '@modules/orders/infra/typeorm/entities/Order';
+import { Product } from '@modules/products/infra/typeorm/entities/Product';
+import { Customer } from '@modules/customers/infra/typeorm/entities/Customer';
+import { UserToken } from '@modules/users/infra/typeorm/entities/UserToken';
+import { OrderProduct } from '@modules/orders/infra/typeorm/entities/OrderProduct';
--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -1,3 +1,33 @@
-import { createConnection } from 'typeorm';
+import { join } from 'node:path';
+import { DataSource, DataSourceOptions } from 'typeorm';
 
-createConnection();
+class DataSourceConfig {
+  private env: string | undefined = process.env.NODE_ENV;
+
+  public config(): DataSourceOptions {
+    const entitiesPath: string = join(__dirname, 'entities', 'index.ts');
+    const migrationsPath: string = join(__dirname, 'migrations');
+
+    if (this.env === 'test') {
+      return {
+        type: 'sqlite',
+        database: ':memory:',
+        synchronize: true,
+        entities: [entitiesPath],
+      };
+    }
+
+    return {
+      type: 'postgres',
+      host: 'pgsqldatabase',
+      port: 5432,
+      username: 'postgres',
+      password: 'docker',
+      database: 'salesapi',
+      entities: [entitiesPath],
+      migrations: [migrationsPath],
+    };
+  }
+}
+
+export const dataSource = new DataSource(new DataSourceConfig().config());
