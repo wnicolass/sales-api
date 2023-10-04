d409852c850bd766aff934d0e1b0992c6c57753b
wnicolass <nicolaswgl99@gmail.com>
2023-10-04 23:17:42 +0100

    feat: update 'UserTokenRepository'

--- src/modules/users/domain/interfaces/IUserTokenRepository.ts
+++ src/modules/users/domain/interfaces/IUserTokenRepository.ts
@@ -1,6 +1,6 @@
 import { IUserToken } from './IUserToken';
 
 export interface IUserTokenRepository {
-  findByToken(token: string): Promise<IUserToken | undefined>;
+  findByToken(token: string): Promise<IUserToken | null>;
   generate(userId: string): Promise<IUserToken>;
 }
--- src/modules/users/infra/typeorm/repositories/UserToken.ts
+++ src/modules/users/infra/typeorm/repositories/UserToken.ts
@@ -1,14 +1,17 @@
-import { Repository, getRepository } from 'typeorm';
+import { Repository } from 'typeorm';
 import { UserToken } from '../entities/UserToken';
+import { dataSource } from '@shared/infra/typeorm';
 import { IUserTokenRepository } from '@modules/users/domain/interfaces/IUserTokenRepository';
 
 export class UserTokenRepository implements IUserTokenRepository {
   constructor(
-    private ormRepo: Repository<UserToken> = getRepository(UserToken),
+    private ormRepo: Repository<UserToken> = dataSource.getRepository(
+      UserToken,
+    ),
   ) {}
 
-  public async findByToken(token: string): Promise<UserToken | undefined> {
-    return await this.ormRepo.findOne({ where: { token } });
+  public async findByToken(token: string): Promise<UserToken | null> {
+    return await this.ormRepo.findOneBy({ token });
   }
 
   public async generate(userId: string): Promise<UserToken> {
